8a01a5e59e48cd853c38bbaeb72fd0219213569a
Alexey Pobery <poberyaa@gmail.com>
2018-08-16 00:17:52 +0300

    Add translation file example

---
+++ code_samples/translation_file_example.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" ?>
+<TS>
+  <context>
+    <name>app</name>
+    <message>
+      <location filename="src/app.qml" line="1"/>
+      <source>Hello world</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+</TS>
