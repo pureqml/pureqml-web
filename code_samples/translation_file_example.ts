1e057766656e5405cb080b2688cdb1ad689261ce
Alexey Pobery <poberyaa@gmail.com>
2018-08-17 22:41:44 +0300

    Update localization ts-example

--- code_samples/translation_file_example.ts
+++ code_samples/translation_file_example.ts
@@ -1,11 +1,11 @@
-<?xml version="1.0" ?>
-<TS>
-  <context>
-    <name>app</name>
-    <message>
-      <location filename="src/app.qml" line="1"/>
-      <source>Hello world</source>
-      <translation type="unfinished"/>
-    </message>
-  </context>
-</TS>
+&lt;?xml version="1.0"? &gt;
+	&lt;TS&gt;
+	&lt;context&gt;
+		&lt;name&gt;app&lt;/name&gt;
+		&lt;message&gt;
+			&lt;location filename=\"src/app.qml\" line=\"1\"/&gt;
+			&lt;source&gt;Hello world&lt;/source&gt;
+			&lt;translation/&gt;Привет мир&lt;/translation/&gt;
+		&lt;/message&gt;
+	&lt;/context&gt;
+&lt;/TS&gt;
